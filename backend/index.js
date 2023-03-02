085478a56efbf62a44a2778f6c3cc4171a77f07d
Thomas <nyumat18@gmail.com>
2023-03-01 19:34:42 -0800

    Register Route connected to server

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,21 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import register from "./routes/register.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Routes
+app.use("/api/register", register);
+
+// Default Route
 app.get("/", (req, res) => {
   res.send("Hello TalkToBeavs!");
 });
