007a10337bf5bc8051d1ac3cf7f5a4a46789bf9a
Thomas <nyumat18@gmail.com>
2023-03-01 18:58:19 -0800

    Added Mongoose Connection Callback

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,31 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 
-
 app.get("/", (req, res) => {
-	res.send("Hello TalkToBeavs!");
+  res.send("Hello TalkToBeavs!");
 });
 
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("[Backend ⚡️]: Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(PORT, () => {
-	console.log(`Server Running on port ${PORT}`);
+  console.log(`[Backend ⚡️] Server Running on port ${PORT}`);
 });
-
-
-
-
-
-
-
