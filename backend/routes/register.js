5d3c0b0aeee3cbb7701da38116cdc946a3336f22
Thomas <nyumat18@gmail.com>
2023-03-01 19:52:57 -0800

    Fixed API Register Route

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -12,33 +12,31 @@ router.post("/", async (req, res) => {
       .pattern(new RegExp("^[a-zA-Z._%+-]+@oregonstate.edu$"))
       .required(),
   });
-  const { error } = schema.validate(req.body);
 
-  if (error) {
-    res.status(400).send(error.details[0].message);
-  }
+  try {
+    const { error } = schema.validate(req.body);
 
-  const user = await User.find({ email: req.body.email });
-  if (user.length > 0) {
-    res.status(400).json({ message: "User already exists" });
-    return;
-  } else {
-    const user = new User({
-      name: req.body.name,
-      password: req.body.password,
-      email: req.body.email,
-    });
+    if (error) {
+      return res.status(400).send(error.details[0].message);
+    }
 
-    await user
-      .save()
-      .then(() => {
-        res
-          .status(200)
-          .json({ message: "User created successfully", user: user });
-      })
-      .catch((err) => {
-        res.status(400).json({ message: "User creation failed", error: err });
+    const user = await User.find({ email: req.body.email });
+
+    if (user.length > 0) {
+      return res.status(400).json({ message: "User already exists" });
+    } else {
+      const user = new User({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
       });
+
+      await user.save();
+
+      return res.status(201).json({ message: `${req.body.name} created` });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
   }
 });
 
