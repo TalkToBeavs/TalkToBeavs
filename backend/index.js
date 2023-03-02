34e8ae6bf6f5895af6ceaa18199ce8619207f866
T0M <46255836+Nyumat@users.noreply.github.com>
2023-03-01 20:12:38 -0800

    [PR-1] Merge Backend with Main (#1)
    * Added Mongoose and Dotenv
    
    * Gitignore update
    
    * Added Mongoose Connection Callback
    
    * Added User Model
    
    * Register Route
    
    * Register Route connected to server
    
    * Added Joi Validation Lib
    
    * Fixed API Register Route

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,39 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+import register from "./routes/register.js";
+
+dotenv.config();
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Routes
+app.use("/api/register", register);
 
+// Default Route
 app.get("/", (req, res) => {
-	res.send("Hello TalkToBeavs!");
+  res.send("Hello TalkToBeavs!");
 });
 
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("[Backend ⚡️]: Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(PORT, () => {
-	console.log(`Server Running on port ${PORT}`);
+  console.log(`[Backend ⚡️] Server Running on port ${PORT}`);
 });
-
-
-
-
-
-
-
