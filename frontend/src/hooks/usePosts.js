8863284e168bf8259e467339eae6fc43f37802a4
solderq35 <solderq35@gmail.com>
2023-03-18 17:05:06 -0700

    access the backend endpoint correctly for searchiing posts by user

--- frontend/src/hooks/usePosts.js
+++ frontend/src/hooks/usePosts.js
@@ -3,35 +3,27 @@ import { useSelector } from 'react-redux';
 import axios from 'axios';
 
 function usePosts({ onid }) {
+  const [posts, setPosts] = useState(null);
 
-      const emailDomain = '@oregonstate.edu'
-      onid = onid + emailDomain
-      const [posts, setPosts] = useState(null)
+  const fetchPosts = async () => {
+    try {
+      const response = await axios.get(`http://localhost:8080/api/feed/get_posts/user?onid=${onid}`);
+      setPosts(response.data.posts);
+    } catch (err) {
+      setPosts(null);
+      console.error(err.response.data.message);
+    }
+  };
 
-      const fetchPosts = async () => {
-            try {
-              const response = await axios.get(
-                `http://localhost:8080/api/feed/get_posts`
-              )
-              const allPosts = response.data.posts[0].posts
-              const filteredPosts = allPosts.filter(post => post.postedBy === onid)
-              setPosts(filteredPosts)
-            } catch (err) {
-              setPosts(null)
-            }
-          }
+  useEffect(() => {
+    fetchPosts();
 
-      useEffect(() => {
-            fetchPosts()
+    return () => {
+      setPosts(null);
+    };
+  }, [onid]);
 
-            return () => {
-                  setPosts(null)
-
-            }
-
-      }, [onid])
-
-      return posts
+  return posts;
 }
 
 export default usePosts;
