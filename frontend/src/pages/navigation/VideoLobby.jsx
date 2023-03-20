8eee09a3021ecfea245449726d598340095544f8
Thomas <nyumat18@gmail.com>
2023-03-20 15:04:10 -0700

    Added Heroku Deployment URL

--- frontend/src/pages/navigation/VideoLobby.jsx
+++ frontend/src/pages/navigation/VideoLobby.jsx
@@ -3,7 +3,7 @@ import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 const VideoLobby = () => {
-  // const socket = io('http://localhost:8080');
+  // const socket = io('https://talk-to-beavs.herokuapp.com');
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
