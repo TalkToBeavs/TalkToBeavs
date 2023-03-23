8316ec6244b2b9be3a545637c51edb2759be1425
solderq35 <solderq35@gmail.com>
2023-03-22 23:27:56 -0700

    mostly feature complete but need to clean up code now

--- frontend/src/components/custom/CreatePostModal.jsx
+++ frontend/src/components/custom/CreatePostModal.jsx
@@ -19,6 +19,7 @@ import {
   ModalHeader,
   ModalOverlay,
   SimpleGrid,
+  Spinner,
   Textarea,
 } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
@@ -31,7 +32,7 @@ export default function CreatePostModal({ isOpen, onClose, handleValidPost }) {
   const [loading, setLoading] = useState(true);
   const [searchValue, setSearchValue] = useState('');
   const [shouldFetch, setShouldFetch] = useState(false);
-  const [menuListHeight, setMenuListHeight] = useState(0);
+  const [shouldFetchTrending, setShouldFetchTrending] = useState(false);
 
   const handleGifClick = (url) => {
     setContent(url);
@@ -39,26 +40,41 @@ export default function CreatePostModal({ isOpen, onClose, handleValidPost }) {
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch(
-        `http://localhost:8080/api/feed/giphy_search?q=${searchValue}`,
-      );
+      const response = await fetch(`http://localhost:8080/api/feed/giphy_search?q=${searchValue}`);
       const json = await response.json();
-      setData(json.data);
+      setData(json.data.data);
       setShouldFetch(false);
+      setLoading(true);
     }
-    console.log(data)
 
     if (shouldFetch) {
       fetchData();
     }
   }, [shouldFetch, searchValue]);
 
+  useEffect(() => {
+    async function fetchDataTrending() {
+      const response = await fetch(`http://localhost:8080/api/feed/giphy_trending`);
+      const json = await response.json();
+      setData(json.data.data);
+      setShouldFetchTrending(false);
+      setLoading(true);
+    }
+    console.log(data);
+
+    if (shouldFetchTrending) {
+      fetchDataTrending();
+    }
+  }, [shouldFetchTrending]);
+
+  /*
   const menuListRef = (node) => {
     if (node !== null) {
       setMenuListHeight(node.offsetHeight);
     }
   };
-
+*/
+  /*
   useEffect(() => {
     const menuListElement = menuListRef.current;
     if (menuListElement) {
@@ -66,7 +82,8 @@ export default function CreatePostModal({ isOpen, onClose, handleValidPost }) {
       setMenuListHeight(scrollHeight - clientHeight);
     }
   }, [data]);
-
+  */
+  /*
   useEffect(() => {
     const menuListElement = menuListRef.current;
     if (menuListElement) {
@@ -79,6 +96,7 @@ export default function CreatePostModal({ isOpen, onClose, handleValidPost }) {
       }
     }
   }, [menuListHeight]);
+  */
 
   const userData = useSelector((state) => state.user.data);
 
@@ -108,13 +126,18 @@ export default function CreatePostModal({ isOpen, onClose, handleValidPost }) {
   };
 
   console.log(searchValue);
+  console.log(loading);
 
   return (
-<Modal isOpen={isOpen} onClose={() => {
-  console.log('Modal closed');
-  onClose();
-  setData('')
-}}>
+    <Modal
+      isOpen={isOpen}
+      onClose={() => {
+        console.log('Modal closed');
+        onClose();
+        setData('');
+        setLoading(true);
+      }}
+    >
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>Create a post</ModalHeader>
@@ -149,6 +172,9 @@ export default function CreatePostModal({ isOpen, onClose, handleValidPost }) {
                     console.log(data);
                     if (!isOpen) {
                       console.log('active');
+                      console.log('search value here: ' + searchValue);
+                      setLoading(true);
+                      setShouldFetchTrending(true);
                     } else {
                       console.log('not active');
                       setData('');
@@ -157,14 +183,22 @@ export default function CreatePostModal({ isOpen, onClose, handleValidPost }) {
                 >
                   {isOpen ? 'Close' : 'Open'}
                 </MenuButton>
-                <MenuList ref={menuListRef}>
+                <MenuList>
                   <Input
                     placeholder='Search for GIFs'
                     onChange={(event) => setSearchValue(event.target.value)}
+                    onKeyDown={(event) => {
+                      if (event.keyCode === 13) {
+                        setShouldFetch(true);
+                        setLoading(true);
+                        setTimeout(() => {
+                          setSearchValue('');
+                        }, 1000);
+                      }
+                    }}
                   />
                   <Button
                     onClick={() => {
-                      console.log(searchValue);
                       setShouldFetch(true);
                       setTimeout(() => {
                         setSearchValue('');
@@ -173,40 +207,57 @@ export default function CreatePostModal({ isOpen, onClose, handleValidPost }) {
                   >
                     Search
                   </Button>
-                  <SimpleGrid columns={5} spacing={0} align='center' justify='center'>
-                    {data &&
-                      data.map(({ id, embed_url }) => (
-                        <MenuItem
-                          key={id}
-                          onClick={() => {
-                            handleGifClick(embed_url);
-                          }}
+                  <Box maxHeight='200px' overflowY='scroll'>
+                    <SimpleGrid columns={5} spacing={0} align='center' justify='center'>
+                      {data &&
+                        data
+                          .filter(
+                            (value, index, self) =>
+                              self.findIndex((v) => v.id === value.id) === index,
+                          )
+                          .map(({ id, embed_url }) => (
+                            <MenuItem key={id} onClick={() => handleGifClick(embed_url)}>
+                              <Box position='relative' height='80px' width='80px'>
+                                <iframe
+                                  src={embed_url}
+                                  width='80'
+                                  height='80'
+                                  frameBorder='0'
+                                  className='giphy-embed'
+                                  allowFullScreen={false}
+                                  style={{ pointerEvents: 'none' }}
+                                  onLoad={() =>
+                                    setTimeout(() => {
+                                      setLoading(false);
+                                    }, 500)
+                                  }
+                                ></iframe>
+                                <div
+                                  style={{
+                                    position: 'absolute',
+                                    top: 0,
+                                    left: 0,
+                                    width: '100%',
+                                    height: '100%',
+                                    zIndex: 1,
+                                    cursor: 'default',
+                                  }}
+                                ></div>
+                              </Box>
+                            </MenuItem>
+                          ))}
+                      {loading && (
+                        <Box
+                          position='absolute'
+                          top='50%'
+                          left='50%'
+                          transform='translate(-50%, -50%)'
                         >
-                          <Box position='relative' height='80px' width='80px'>
-                            <iframe
-                              src={embed_url}
-                              width='80'
-                              height='80'
-                              frameBorder='0'
-                              className='giphy-embed'
-                              allowFullScreen={false}
-                              style={{ pointerEvents: 'none' }}
-                            ></iframe>
-                            <div
-                              style={{
-                                position: 'absolute',
-                                top: 0,
-                                left: 0,
-                                width: '100%',
-                                height: '100%',
-                                zIndex: 1,
-                                cursor: 'default',
-                              }}
-                            ></div>
-                          </Box>
-                        </MenuItem>
-                      ))}
-                  </SimpleGrid>
+                          <Spinner size='xl' color='lightblue' thickness='5px' />
+                        </Box>
+                      )}
+                    </SimpleGrid>
+                  </Box>
                 </MenuList>
               </>
             )}
@@ -220,6 +271,7 @@ export default function CreatePostModal({ isOpen, onClose, handleValidPost }) {
               setSearchValue('');
               setData('');
               handleClose();
+              setLoading(true);
             }}
           >
             Cancel
