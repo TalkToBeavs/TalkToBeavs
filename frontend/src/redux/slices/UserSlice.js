d1eb7982648521a5d72a83121e1f6e149b072215
Jeff H <82061589+solderq35@users.noreply.github.com>
2023-03-23 02:52:50 -0700

    Revert "Giphy branch (#21)"
    This reverts commit f7e71a09d2781ad7c6ec4dbae95fe32638501199.

--- frontend/src/redux/slices/UserSlice.js
+++ frontend/src/redux/slices/UserSlice.js
@@ -12,7 +12,7 @@ export const loadUserData = createAsyncThunk(
   'user/loadUserData',
   async (email, { rejectWithValue }) => {
     try {
-      const response = await axios.get(`http://localhost:8080/api/auth/load_user?email=${email}`);
+      const response = await axios.get(`https://talk-to-beavs.herokuapp.com/api/auth/load_user?email=${email}`);
       return response.data;
     } catch (err) {
       return rejectWithValue(err.response.data);
@@ -24,7 +24,7 @@ export const registerUser = createAsyncThunk(
   'user/register',
   async (values, { rejectWithValue }) => {
     try {
-      const response = await axios.post('http://localhost:8080/api/auth/register', values);
+      const response = await axios.post('https://talk-to-beavs.herokuapp.com/api/auth/register', values);
       return response.data;
     } catch (err) {
       return rejectWithValue(err.response.data);
