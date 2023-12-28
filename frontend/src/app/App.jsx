214ea105b1f42b3e7e9b8d3a33a2de77d9cc5057
Thomas <nyumat18@gmail.com>
2023-12-28 03:18:41 -0800

    Fixed Protected Route Redirects

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -21,44 +21,53 @@ import Login from '../pages/auth/Login';
 import Signup from '../pages/auth/Signup';
 
 import { AnimatePresence } from 'framer-motion';
+import { useEffect } from 'react';
 import TextChat from '../pages/chat/TextChat';
 import VideoChat from '../pages/chat/VideoChat';
-import Profile from '../pages/profile/Profile';
 import Edit from '../pages/profile/Edit';
+import Profile from '../pages/profile/Profile';
 
 const App = () => {
-  const location = useLocation();
-  return (
-    <Provider store={store}>
-      <AnimatePresence mode='wait'>
-        <SidebarWithHeader>
-          <ThemeToggle />
-          <Routes>
-            <Route path='/' element={<Landing />} />
-            <Route path='/signup' element={<Signup />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/logout' element={<Logout />} />
+    const location = useLocation();
+    const protectedRoutes = ['/home', '/profile', '/feed', '/lobby', '/video', '/text', '/edit'];
+    const isProtectedRoute = protectedRoutes.includes(location.pathname);
+    const isAuthed = localStorage.getItem('token');
+    useEffect(() => {
+        if (isProtectedRoute && !isAuthed) {
+            window.location.href = '/';
+        }
+    }, [location]);
+    return (
+        <Provider store={store}>
+            <AnimatePresence mode='wait'>
+                <SidebarWithHeader>
+                    <ThemeToggle />
+                    <Routes>
+                        <Route path='/' element={<Landing />} />
+                        <Route path='/signup' element={<Signup />} />
+                        <Route path='/login' element={<Login />} />
+                        <Route path='/logout' element={<Logout />} />
 
-            <Route path='/home' element={<Home />} />
+                        <Route path='/home' element={<Home />} />
 
-            <Route path='/profile/:onid' element={<Profile />} />
-            <Route path='/edit' element={<Edit />} />
+                        <Route path='/profile/:onid' element={<Profile />} />
+                        <Route path='/edit' element={<Edit />} />
 
-            <Route path='/feed' element={<Feed />} />
-            <Route path='/lobby' element={<Lobby />} />
+                        <Route path='/feed' element={<Feed />} />
+                        <Route path='/lobby' element={<Lobby />} />
 
-            <Route path='/video' element={<VideoLobby />} />
-            <Route path='/video/:roomId' element={<VideoChat />} />
+                        <Route path='/video' element={<VideoLobby />} />
+                        <Route path='/video/:roomId' element={<VideoChat />} />
 
-            <Route path='/text' element={<TextLobby />} />
-            <Route path='/text/:roomId' element={<TextChat />} />
+                        <Route path='/text' element={<TextLobby />} />
+                        <Route path='/text/:roomId' element={<TextChat />} />
 
-            <Route path='*' element={<FourOhFour />} />
-          </Routes>
-        </SidebarWithHeader>
-      </AnimatePresence>
-    </Provider>
-  );
+                        <Route path='*' element={<FourOhFour />} />
+                    </Routes>
+                </SidebarWithHeader>
+            </AnimatePresence>
+        </Provider>
+    );
 };
 
 export default App;
