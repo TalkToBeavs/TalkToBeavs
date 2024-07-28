52c52644118c3b8ac5d8f8ed0300589ef8f3f2ac
Jake Gehrke <gehrkej@oregonstate.edu>
2024-07-27 22:29:33 -0700

    add - admin panel for user management

---
+++ frontend/src/components/admin/Users.tsx
@@ -0,0 +1,62 @@
+import { Box, Flex, Input, Select, StackDivider, useDisclosure, VStack } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import UserOptionsModal from '../custom/UserOptionsModal';
+import UserCard from './UserCard';
+
+import { User, users } from '../../data/Users';
+
+const Users: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filter, setFilter] = useState('');
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+
+  const { isOpen: userIsOpen, onOpen: userOnOpen, onClose: userOnClose } = useDisclosure();
+
+  const handleOptionsClick = (user: User) => {
+    console.log(`Options clicked for ${user.username}`);
+    setSelectedUser(user);
+    userOnOpen();
+  };
+
+  return (
+    <>
+      <UserOptionsModal isOpen={userIsOpen} onClose={userOnClose} user={selectedUser} />
+      <Box p='4'>
+        {/* Search and Filters */}
+        <Flex mb='4' align='center'>
+          <Input
+            placeholder='Search users'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            mr='4'
+            maxWidth='400px'
+          />
+          <Select
+            placeholder='Select filter'
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+            mr='4'
+            maxWidth='200px'
+          >
+            <option value='filter1'>Filter 1</option>
+            <option value='filter2'>Filter 2</option>
+          </Select>
+        </Flex>
+
+        {/* User Cards */}
+        <VStack divider={<StackDivider borderColor='gray.200' />} spacing={4} align='stretch'>
+          {' '}
+          {users.map((user) => (
+            <UserCard
+              key={user.username}
+              username={user.username}
+              onOptionsClick={() => handleOptionsClick(user)}
+            />
+          ))}
+        </VStack>
+      </Box>
+    </>
+  );
+};
+
+export default Users;
