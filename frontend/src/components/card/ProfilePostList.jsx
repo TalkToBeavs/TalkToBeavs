9b54df8ca7bc0a1097b2948bedfddca65bddb0bc
Jake Gehrke <gehrkej@oregonstate.edu>
2024-07-30 10:27:15 -0700

    add - added functionality for post report modal

--- frontend/src/components/card/ProfilePostList.jsx
+++ frontend/src/components/card/ProfilePostList.jsx
@@ -1,8 +1,8 @@
 import { Box, useMediaQuery } from '@chakra-ui/react';
-import Post from './Post';
 import { useSelector } from 'react-redux';
+import Post from './Post';
 
-export default function ProfilePostList({ posts }) {
+export default function ProfilePostList({ posts, handleReportOpening }) {
   const [isMobile] = useMediaQuery('(max-width: 768px)');
   const user = useSelector((state) => state.user.data);
   return (
@@ -16,7 +16,10 @@ export default function ProfilePostList({ posts }) {
         ml={isMobile ? 0 : 8}
         mt={4}
       >
-        {posts && posts.map((post, i) => <Post key={i} user={user} post={post} />)}
+        {posts &&
+          posts.map((post, i) => (
+            <Post key={i} user={user} post={post} handleReportOpening={handleReportOpening} />
+          ))}
       </Box>
     )
   );
